4597408cf76c8e968495d080e5aefce6e2edece8
Anas Almalki <contact_anasalmalki@gmail.com>
2026-07-10 08:42:53 +0300

    Chapter 3: Lists

--- lists.py
+++ lists.py
@@ -1,10 +1,10 @@
-components=["valve","pump","reactor","piping"]
+components = ["valve", "pump", "reactor", "piping"]
 print(components[0])
 print(components[-1])
 print(f"the {components[0]} needs maintenance.".title())
-components[0]="control_valve"
+components[0] = "control_valve"
 components.append("compressor")
-components.insert(1,"heat_exchanger")
+components.insert(1, "heat_exchanger")
 print(components)
 del components[0]
 components.pop()
@@ -12,7 +12,7 @@ last_removed = components.pop()
 print(f"successfully popped: {last_removed}".capitalize())
 components.remove("reactor")
 print(components)
-cities=["Dammam","Khobar","Riyadh","Jeddah","Jubail"]
+cities = ["Dammam", "Khobar", "Riyadh", "Jeddah", "Jubail"]
 print(len(cities))
 print(sorted(cities))
 print(cities)
@@ -20,4 +20,3 @@ cities.sort()
 print(cities)
 popped_city = cities.pop()
 print(popped_city)
-#

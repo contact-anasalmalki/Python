dcb8c9877236f4ee7c7d8cd8e5c2df2e98ee5227
Anas Almalki <contact_anasalmalki@gmail.com>
2026-07-10 08:58:14 +0300

    Chapter 4: Working with Lists

--- loops.py
+++ loops.py
@@ -3,6 +3,7 @@ materials = ["nitrogen", "oxygen", "argon"]
 for material in materials:
     print(f"Analyzing sample: {material.title()}")
 print("Analysis is complete.")
+
 # range() function generates a sequence of numbers, starting from 0 by default, and increases by 1 (by default), and stops before a specified number.
 squares = []
 for y in range(1, 11):
@@ -12,6 +13,7 @@ print(squares)
 print(min(squares))
 print(max(squares))
 print(sum(squares))
+
 # list comprehension (shortcut) to create a list of squares
 squares_shortcut = [x**2 for x in range(1, 11)]
 print(squares_shortcut)
@@ -21,6 +23,7 @@ print(squares_shortcut[-3:])
 squares_copy = squares_shortcut[:]
 squares_copy.append(999)
 print(f"{squares_shortcut}{squares_copy}")
+
 # List (tuples) can not be changed, but they can be reassigned to a new value.
 standard_conditions = (273.15, 101.325)
 print(standard_conditions[0:2])

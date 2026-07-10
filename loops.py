5d126c711384c38d5d347cbb4a92ed63b6bc66b5
Anas Almalki <contact_anasalmalki@gmail.com>
2026-07-10 08:57:30 +0300

    Chapter 4: Working with Lists

--- loops.py
+++ loops.py
@@ -1,8 +1,9 @@
+# for loop is used to iterate over a sequence (list, tuple, dictionary, set, or string).
 materials = ["nitrogen", "oxygen", "argon"]
 for material in materials:
     print(f"Analyzing sample: {material.title()}")
 print("Analysis is complete.")
-
+# range() function generates a sequence of numbers, starting from 0 by default, and increases by 1 (by default), and stops before a specified number.
 squares = []
 for y in range(1, 11):
     x = y**2
@@ -11,7 +12,7 @@ print(squares)
 print(min(squares))
 print(max(squares))
 print(sum(squares))
-
+# list comprehension (shortcut) to create a list of squares
 squares_shortcut = [x**2 for x in range(1, 11)]
 print(squares_shortcut)
 print(squares_shortcut[:4])
@@ -20,7 +21,7 @@ print(squares_shortcut[-3:])
 squares_copy = squares_shortcut[:]
 squares_copy.append(999)
 print(f"{squares_shortcut}{squares_copy}")
-
+# List (tuples) can not be changed, but they can be reassigned to a new value.
 standard_conditions = (273.15, 101.325)
 print(standard_conditions[0:2])
 # standard_conditions[0] = 298.15

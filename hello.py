4b8fa7f57853f5ea805698ac77b70b26f185dc35
Anas Almalki <contact_anasalmalki@gmail.com>
2026-07-10 08:36:59 +0300

    Chapter 1: Introduction & First Script

--- hello.py
+++ hello.py
@@ -11,13 +11,3 @@ print(full_name.title())
 favorite_language = " python "
 print(f"original:'{favorite_language}'")
 print(f'stripped:"{favorite_language.strip()}"')
-mass = 80
-velocity = 12.5
-kinetic_energy = 0.5 * mass * velocity**2
-print(kinetic_energy)
-GRAVITY = 9.8
-mass = 10
-height = 5
-# Calculate Potential Energy
-potential_energy = mass * GRAVITY * height
-print(potential_energy)
